bb70e5baf4fb3d150c9315d2659da2701231ca66
Uday Teja <udayakaramsetty@gmail.com>
2024-12-19 13:57:24 +0530

    the 1st commit

---
+++ mongodb/mongo.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+#user check 
+TIME=$(date +%F-%H-%M-%S)
+LOGFILE=/"tmp/$0-$TIME.log"
+
+ID =$(id -u)
+
+if [ $ID -ne 0 ]
+then 
+    echo "switch to the root user"
+    exit 1
+else
+    echo "root user"
+
+VALIDATE ()
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2...failed"
+    else
+        echo "$2..success"
+}
+
+cp mongo.repo /etc/yum.repos.d &>> $LOGFILE
+VALIDATE $? "copying the mongodb"
+
+dnf install mongodb-org -y &>> $LOGFILE
+VALIDATE $? "installing mongodb"
+
+systemctl enable mongod &>> $LOGFILE
+VALIDATE $? "enabling mongodb" 
+
+systemctl start mongod &>> $LOGFILE
+VALIDATE $? "starting mongodb" 
+
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf  &>> $LOGFILE
+VALIDATE $? "navigating the mongodb" 
+
+systemctl restart mongod
+VALIDATE $? "restarting the mongodb" &>> $LOGFILE 
