89f7884d17f6764b8a9d71ea1d74228d741d3a25
Uday Teja <udayakaramsetty@gmail.com>
2024-12-24 13:26:56 +0530

    new folders

---
+++ redis/redis.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#user check 
+TIME=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIME.log"
+exec &> LOGFILE
+
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then 
+    echo "switch to the root user"
+    exit 1
+else
+    echo "root user"
+fi
+
+VALIDATE ()
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2...is failed"
+    else
+        echo "$2.. is success"
+    fi
+}
+
+dnf install redis -y
+VALIDATE $? "installing redis"
+
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/redis/redis.conf 
+VALIDATE $? "changing the ip in the redis"
+
+systemctl enable redis
+VALIDATE $? "enabling redis"
+
+systemctl start redis
+VALIDATE $? "starting redis"
---
+++ user/user.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+#user check 
+TIME=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIME.log"
+MONGOIP=
+REDISIP=
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then 
+    echo "switch to the root user"
+    exit 1
+else
+    echo "root user"
+fi
+
+VALIDATE ()
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2...is failed"
+    else
+        echo "$2.. is success"
+    fi
+}
+
+
+
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "disabling the nodejs current version"
+
+dnf module enable nodejs:20 -y &>> $LOGFILE
+VALIDATE $? "enabling the nodejs current version"
+
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE $? "Installing the current nodejs version"
+
+id roboshop &>> $LOGFILE
+
+if [ $? -ne 0 ]
+then 
+    useradd roboshop &>> $LOGFILE
+    VALIDATE $? "useradd user creation "
+else 
+    echo "user creation skipping"
+fi 
+
+mkdir -p /app &>> $LOGFILE
+VALIDATE $? "the app directory creation"
+
+curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip  &>> LOGFILE
+VALIDATE $? "installing the zip file"
+
+cd /app &>> LOGFILE
+VALIDATE $? "redirecting to the  the app directory"
+
+unzip -o /tmp/user.zip  &>> LOGFILE
+VALIDATE $? "unzipping the files in the app directory"
+
+cd /app &>> LOGFILE
+VALIDATE $? "redirecting to the  the app directory"
+
+npm install &>> LOGFILE
+VALIDATE $? "installing the dependencies"
+
+cp /home/ec2-user/user/user.service /etc/systemd/system/user.service  &>> LOGFILE
+VALIDATE $? "copying the files to sytemd "
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "reloading the service"
+
+systemctl enable user &>> $LOGFILE
+VALIDATE $? "enabling the service"
+
+systemctl start user &>> $LOGFILE
+VALIDATE $? "starting the service"
+
+
+cp  /home/ec2-user/roboshop-new/catalogue/mongo.repo /etc/yum.repos.d &>> $LOGFILE
+VALIDATE $? "creating mongo repo"
+
+dnf install -y mongodb-mongosh &>> $LOGFILE
+VALIDATE $? "installing mongodb shell"
+
+
+mongosh --host $MONGOIP </app/schema/catalogue.js &>> $LOGFILE
+VALIDATE  $? "loading catalogue data in mongodb"
