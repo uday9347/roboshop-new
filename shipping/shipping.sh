fe1c43212b05e10b35cf8a8e1a3296445a56e529
Uday Teja <udayakaramsetty@gmail.com>
2024-12-24 17:50:49 +0530

    new

--- shipping/shipping.sh
+++ shipping/shipping.sh
@@ -2,7 +2,7 @@
 
 #user check 
 TIME=$(date +%F-%H-%M-%S)
-CRED=$(-uroot -pRoboShop@1)
+CRED=-uroot -pRoboShop@1
 
 LOGFILE="/tmp/$0-$TIME.log"
 
@@ -75,7 +75,7 @@ systemctl start shipping
 dnf install mysql -y &>> $LOGFILE
 VALIDATE $? "installing sql "
 
-mysql -h 172.31.21.180 $CRED < /app/db/schema.sql
+mysql -h 172.31.21.180 $CRED< /app/db/schema.sql
 
 mysql -h 172.31.21.180 $CRED< /app/db/app-user.sql 
 
